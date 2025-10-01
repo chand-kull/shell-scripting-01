158bc8147a77420d1029ac4069a0eb9bc161e8bd
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-01 22:42:34 +0530

    this is my first commit

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash 
+
+
+
+
+echo "chandu:: hey hai ra ram , how are you ?.
+echo " ram  :: hi chandu, i am fine . how are you ?.
+echo "chandu:: i am fine too . how is you work?.
+echo "rem   :: not bad. i am thinking to upgrade to DevOps"..............
