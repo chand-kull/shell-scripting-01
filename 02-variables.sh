45811b18fcc6ccafd686fecc212722b06fec4e28
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-01 22:45:55 +0530

    helo

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 
 
 
-echo "chandu:: hey hai ra ram , how are you ?.
-echo " ram  :: hi chandu, i am fine . how are you ?.
-echo "chandu:: i am fine too . how is you work?.
-echo "rem   :: not bad. i am thinking to upgrade to DevOps"..............
+echo "chandu:: hey hai ra ram , how are you ?"
+echo " ram:: hi chandu, i am fine . how are you ?"
+echo "chandu:: i am fine too . how is you work?"
+echo "rem :: not bad. i am thinking to upgrade to DevOps"
