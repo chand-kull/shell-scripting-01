e821e9e2f7ca5d66168d6aa3990a33bcbbab1b95
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-01 23:02:02 +0530

    helo

--- 04-variables.sh
+++ 04-variables.sh
@@ -8,6 +8,6 @@ PERSON2=$2
 
 #referring variable
 echo "$PERSON1:: hey hai ra $PERSON2 , how are you ?"
-echo "$PERSON2  :: hi $PERSON1, i am fine . how are you ?"
+echo "$PERSON2:: hi $PERSON1, i am fine . how are you ?"
 echo "$PERSON1:: i am fine too . how is you work?"
-echo "$PERSON2  :: not bad. i am thinking to upgrade to DevOps"
+echo "$PERSON2:: not bad. i am thinking to upgrade to DevOps"
