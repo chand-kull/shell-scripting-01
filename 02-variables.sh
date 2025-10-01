8b3d0850c4d7f79cadba0095ca055e88df12e3b7
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-01 22:46:48 +0530

    helo

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@
 
 
 echo "chandu:: hey hai ra ram , how are you ?"
-echo " ram:: hi chandu, i am fine . how are you ?"
+echo "ram   :: hi chandu, i am fine . how are you ?"
 echo "chandu:: i am fine too . how is you work?"
-echo "rem :: not bad. i am thinking to upgrade to DevOps"
+echo "rem   :: not bad. i am thinking to upgrade to DevOps"
