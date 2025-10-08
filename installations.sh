325e6eadbcc65cc5b4e450e80c1edaaaba13c902
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-08 15:51:40 +0530

    helo

--- installations.sh
+++ installations.sh
@@ -5,9 +5,10 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access"
+    exit 1 #manually exit if error comes.
     else
         echo "you are syper user."
-        exit 1 #manually exit if error comes.
+
 fi
 
 yum install mysql -y
