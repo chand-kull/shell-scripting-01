717790f54b65779580237a77b3647cb0416c400a
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-08 09:24:36 +0530

    helo

---
+++ 06variables.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+
+PERSON1=ramesh
+PERSON2=chandu
+
+echo "$PERSON1 :: hey hai $PERSON2 how are you annadu $PERSON2 
+echo "$PERSON2 :: hey hai $PERSON2 i am good what about you annadu $PERSON1
+echo "$PERSON2 :: hey where are you from $PERSON1
+echo "$PERSON1 :: i am from basically andhra pradesh 
+echo "$PERSON2 :: $PERSON1 are you using this application evrey day
+echo "$PERSON1 :: $PERSON2 yes $PERSON1 iam usint this application every day 
