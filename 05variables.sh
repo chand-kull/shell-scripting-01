9d1b9630f242e9ac8222c8cf379921064e8ceb95
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-09 23:13:34 +0530

    helo

---
+++ 05variables.sh
@@ -0,0 +1,13 @@
+#!bin/bash 
+
+echo " please enter username::"
+
+read USERNAME #HEAR USERNAME IS VARIABLE 
+
+echo "please enter password::"
+
+read PASSWORD
+
+echo "username is: $USERNAME, password is : $PASSWORD"
+
+
