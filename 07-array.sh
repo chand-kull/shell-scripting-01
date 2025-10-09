248136ea66b62926437d43acc6785e46496179da
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-09 23:34:12 +0530

    helo

--- 07-array.sh
+++ 07-array.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ITEMS=("CAR" "FLIGHT"  )
+ITEMS=("CAR" "FLIGHT" "BICKES")
+
+
+#size of above array is 3 
+#index are 0, 1, 2
+
+#list always starts with 0.
+
+echo "First item is : ${car}"
+echo "second item is: ${flight}"
+echo "third item is : ${bickes}"
     
