1c590e1d4c4016063106bc352abf0ba3420f6c97
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-09 23:39:52 +0530

    helo

--- 07-array.sh
+++ 07-array.sh
@@ -8,7 +8,7 @@ ITEMS=("CAR" "FLIGHT" "BICKES")
 
 #list always starts with 0.
 
-echo "First item is : ${ITEMS}"
-echo "second item is: ${ITEMS}"
+echo "First item is : ${ITEMS[0]}"
+echo "second item is: ${ITEMS[1]}"
 echo "third item is : ${ITEMS[@]}"
     
