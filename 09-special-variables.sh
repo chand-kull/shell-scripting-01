621e5f1742974a872d67e4bb79a1c8da68ad0d7b
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-10 06:02:59 +0530

    some-massage

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -5,4 +5,5 @@ echo "Number of variables passed : $#
 echo " script name : $0
 echo "corrext  working directory : $PWD
 echo " home directory of currenct user : $HOME"
-echo " 
+echo " which user is running this sript : $USER"
+
