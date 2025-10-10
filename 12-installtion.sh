44bf392f2068357a01624adf609c4bbdc33db2d8
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-10 12:16:40 +0530

    hey chandu this is data types

---
+++ 12-installtion.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access."
+    exit 1
+else
+    echo "you are super user."
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]  
+then
+    echo "installation of git .....failure"
+    exit 1
+else 
+    echo "installation of git ........success."    
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]  
+then
+    echo "installation of mysql .....failure"
+    exit 1
+else 
+    echo "installation of mysql ........success."    
+fi
