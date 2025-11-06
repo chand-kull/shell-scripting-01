ffd861492d347492b9818fc6c794a7651343f07f
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-06 12:31:36 +0530

    shell

--- functions.sh
+++ functions.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
- # first check super user r not 
- # if super user proceed ,if not should throw  error
-
+ 
  USERID=$(id -u) 
 
  VALIDATE(){
@@ -10,7 +8,7 @@
 
  }
 
- if [$USERID -ne 0] # here it seems exit status= 0 and root user id of root =0 
+ if [$USERID -ne 0] 
  then
   echo "please run this scipt with root acess"
   exit 1 # manully exit if error comes
@@ -19,7 +17,10 @@ else
 fi
 
 dnf install mysql -y
-VALIDATE $? "installing MYSQL
+VALIDATE $? "installing mysql"
+
+dnf install git -y
+VALIDATE $? "installing MYSQL"
  
 
 
