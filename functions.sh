23834c2b0e2921b95dc57d8fc450bd3f85451425
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-06 12:54:52 +0530

    shell

--- functions.sh
+++ functions.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
- 
- USERID=$(id -u) 
-
- VALIDATE(){
-    echo "exit status : $1"
-    echo "what are u doing : $2"
-
- }
-
- if [$USERID -ne 0] 
- then
+USERID=$(id -u) 
+if [$USERID -ne 0] 
+then
   echo "please run this scipt with root acess"
   exit 1 # manully exit if error comes
 else
  echo "super user"
 fi
 
+VALIDATE(){
+    echo "exit status : $1"
+    echo "what are u doing : $2"
+
+ }
+
+
 dnf install mysql -y
 VALIDATE $? "installing mysql"
 
 dnf install git -y
-VALIDATE $? "installing MYSQL"
+VALIDATE $? "installing git"
  
 
 
