f322cf10fd61fd5634bad8c2d04d185d448309be
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-06 12:25:57 +0530

    shell

--- functions.sh
+++ functions.sh
@@ -2,13 +2,14 @@
  # first check super user r not 
  # if super user proceed ,if not should throw  error
 
- USERID=$(id -u) # 1001
+ USERID=$(id -u) 
 
  VALIDATE(){
     echo "exit status : $1"
     echo "what are u doing : $2"
 
  }
+
  if [$USERID -ne 0] # here it seems exit status= 0 and root user id of root =0 
  then
   echo "please run this scipt with root acess"
