1ef3da9d11886bc8c56856160972e18dceb94eca
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-06 15:08:10 +0530

    shell

--- functions.sh
+++ functions.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 USERID=$(id -u) 
+TIMESTAMP=$(date +%F-%H-%M-%S) # to check time
+SCRIPT_NAME=$($0 | cut -d "." -f1) #script name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #to save script name with time
+
 if [$USERID -ne 0] 
 then
   echo "please run this scipt with root acess"
   exit 1 # manully exit if error comes
 else
  echo "super user"
+fi
+# dn install nginx -y
+if [$? -ne 0]
+then 
+ echo "instalin nginx is failure"
+else 
+ echo" installing nginx is success"
 fi
 
 VALIDATE(){
@@ -18,10 +29,10 @@ VALIDATE(){
    fi
 }
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE # store the output in script name with time file
 VALIDATE $? "installing mysql"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing git"
  
 
