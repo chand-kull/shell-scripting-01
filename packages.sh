00a570042120d9936879229fa0fca83d296db68f
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-08 15:19:55 +0530

    installes

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+USERID=$(id -u)
+if [$USERID -ne 0] # here it seems exit status= 0 and root user id of root =0 
+ then
+  echo "please run this scipt with root acess"
+  exit 1 # manully exit if error comes
+else
+ echo "super user"
+fi
+
+echo "all packages: $@"
