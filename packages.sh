ee6bdbe2ff416b6a88b5ad0c5c044c84229c0503
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-08 15:35:37 +0530

    installes

--- packages.sh
+++ packages.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S) # to check time
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #script name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #to save script name with time
+
+
+
 if [$USERID -ne 0] # here it seems exit status= 0 and root user id of root =0 
- then
+then
   echo "please run this scipt with root acess"
   exit 1 # manully exit if error comes
 else
  echo "super user"
 fi
 
-echo "all packages: $@"
+validate(){
+ if [ $1 -ne 0 ]
+  echo "$2.....FAILURE"
+  exit 1
+else
+ echo "$2....SUCCESS"
+fi
+}
+
+for i in $@
+do 
+ echo "packages to install :$i"
+ dnf list installed $i &>>$LOGFILE
+ if [ $? -eq 0 ]
+ then
+  echo "$i already installed ......SKIPPING"
+ else
+  echo "$i not installed.... NEED TO INSTALl"
+ fi
+done
