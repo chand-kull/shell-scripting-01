c62caedd426c2c78404ce9f25e8f8748c16b87c5
chandu <cheerlachandrasekhar81@gmail.com>
2025-11-12 19:10:06 +0530

    comit

--- current script.sh
+++ current script.sh
@@ -4,6 +4,7 @@ COURSE="devops from curent script"
 echo "before calling other script, course: $COURSE"
 echo "process ID of currecnt scipt: $$"
 
-./otherscript.sh
+#  1 way./otherscript.sh
+source ./otherscript.sh #2 nd way
 echo "after calling other script course: $COURSE"
 
