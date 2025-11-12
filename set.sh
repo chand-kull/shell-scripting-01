825f53d1ce222881229616e7064982dee3f4a823
chandu <cheerlachandrasekhar81@gmail.com>
2025-11-13 00:40:58 +0530

    expense

--- set.sh
+++ set.sh
@@ -6,17 +6,18 @@
  failure(){
     echo "Failed at $1:$2"
  }
+ 
  trap 'failed ${LINENO} "$BASH_COMMAND"' ERR #to identify which line number error got
 
- USERID=$(id -u) #ERR
+ USERID=$(id -u)
  
 
  if [ $USERID -ne 0 ] 
  then
-  echo "please run this scipt with root acess"
+  echo "please run this script with root access"
   exit 1 # manully exit if error comes
  else
- echo "super user"
+  echo "super user"
  fi
 
  
