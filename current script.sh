dbc5e2e0286660b76938509db2eae07968d4e77b
chandugit config --global user.email cheerlachandrasekhar81@gmail.com <syedfarzana589@gmail.com>
2025-11-12 17:41:29 +0530

    shell

--- current script.sh
+++ current script.sh
@@ -4,6 +4,6 @@ COURSE="devops from curent script"
 echo "before calling other script, course: $COURSE"
 echo "process ID of currecnt scipt: $$"
 
-./other script.sh
+./otherscript.sh
 echo "after calling other script course: $COURSE"
 
