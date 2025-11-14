3dd1194668e26cf4a25277460e02c585808c92c0
chandu <cheerlachandrasekhar81@gmail.com>
2025-11-14 13:57:32 +0530

    database-server

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT| grep xfs)
+DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 
 while IFS= read -r line
-do 
-  USAGE=$(echo $line | awk -F " " '{print $6}' | cut -d "%" -f1 )                                                                                     d "%" -f1
-  FOLDER=$(echo $line | awk -F " " '{print $NF}')
-  if [ $USAGE -ge $DISK_THRESHOLD ]
-  then 
-    echo "$FOLDER is more than $DISK_THRESHOLD , current usage: $USAGE"
-  fi
-done <<< $DISK_USAGE
+do
+  USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+  FOLDER=$(echo $line | awk '{print $NF}')
 
+  if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
+  then
+    echo "$FOLDER is more than $DISK_THRESHOLD%, current usage: $USAGE%"
+  fi
+done <<< "$DISK_USAGE"
 
 
 
